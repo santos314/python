722eeb81c0af17b949d4820c8b18cdb3d8716008
kaique <kaique.souza.santos@escola.pr.gov.br>
2025-05-07 10:46:59 -0300

    Revert "cap 2 expressoes e instrucoes"
    This reverts commit 4f816a3c93a1642cd7a1999626bf8cf32eac468b.

--- pythonProject1/CAP 2/ex2nomesvariaveis.py
+++
@@ -1,5 +0,0 @@
-#VARIAVEIS NÃO COMEÇAM COM NÚMEROS NEM COM LETRAS MAIUSCÚLAS
-     #76trombones = 'big parade'
-     #Trombones = 'big parade'       #duas variaveis erradas
-
-#EM UMA VARIAVEL NÃO PODE TER CARACTER ESPECIAL(ex *)EXCETO _anderline
