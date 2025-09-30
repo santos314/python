e2bb522c15eef5c2966d3705d5fa26cf338745c3
kevyn <kevyn.paiva@escola.pr.gov.br>
2025-09-30 11:54:45 -0300

    exercicios prova

---
+++ funçoes de leitura/exercicio prova/exeercicio 3.py
@@ -0,0 +1,11 @@
+n1 = int(input("digite o primeiro número: "))
+n2 = int(input("digite o segundo número: "))
+n3 = int(input("digite o terceiro número: "))
+
+if n1 > n2 and n1 > n3:
+    print("o numero 1 é maior")
+if n2 > n1 and n2 > n3:
+    print("o numero 2 é maior")
+if n3 > n1 and n3 > n2:
+    print("o numero 3 é maior")
+
---
+++ funçoes de leitura/exercicio prova/exercicio 2.py
@@ -0,0 +1,8 @@
+def operacao(numero):
+    if numero > 0:
+     print(f"o deu número ao quadrado é: ", numero ** 2)
+
+numero = float(input("digite um numero qualquer: "))
+
+operacao(numero)
+
